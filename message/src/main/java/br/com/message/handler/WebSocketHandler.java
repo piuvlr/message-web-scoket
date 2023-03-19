b486fef74d7d21db202a3f5964d5940057eb9647
Caio Simas <caio.simas@valid.com>
2023-03-19 16:41:43 -0300

    backend enviando eventos

--- message/src/main/java/br/com/message/handler/WebSocketHandler.java
+++ message/src/main/java/br/com/message/handler/WebSocketHandler.java
@@ -1,5 +1,10 @@
 package br.com.message.handler;
 
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -13,6 +18,22 @@ public class WebSocketHandler extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		System.out.println("afterConnectionEstablished - Session ID: " + session.getId());
+		
+		/*
+		 * Apenas para gins de teste
+		 * Função de timer para cliet receber mensagens
+		 *  */
+		
+		new Timer().scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					session.sendMessage(new TextMessage("Olá " + UUID.randomUUID()));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}, 2000, 200);
 	}
 	
 	//Método chamado toda vez que a gente recebe uma mensagem
